f377a97c587180bc2a08dbfc2da5a464b364ee9d
Husnain Raza <hr_11@hotmail.co.uk>
2024-07-10 16:41:39 +0000

    added products,links,search function,images

---
+++ products/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('product_list'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
+
