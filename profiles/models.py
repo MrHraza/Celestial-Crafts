64e0d3c2baa668ce56f805d9aab5b24110ef70b7
Husnain Raza <hr_11@hotmail.co.uk>
2024-08-06 17:47:09 +0000

    fixed sign up bug - integrity error

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from checkout.models import Order
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,10 +18,3 @@ class UserProfile(models.Model):
 
     def get_order_history(self):
         return Order.objects.filter(email=self.user.email).order_by('-date')
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
-        instance.userprofile.save()
