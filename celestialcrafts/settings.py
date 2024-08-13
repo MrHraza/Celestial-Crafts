6ab8da5957fe3af8bae6cdf05312f491271a762e
Husnain Raza <hr_11@hotmail.co.uk>
2024-08-13 08:25:24 +0000

    update

--- celestialcrafts/settings.py
+++ celestialcrafts/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '7kor$v2xukjkn#2g#=n$$2i)!5!thr(6$dxei3=sif_#n!-(sg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
