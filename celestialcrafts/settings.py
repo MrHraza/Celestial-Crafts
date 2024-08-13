c6dcc32b23503c455f539b7ed265030dbe7f502e
Husnain Raza <hr_11@hotmail.co.uk>
2024-08-13 08:31:34 +0000

    update

--- celestialcrafts/settings.py
+++ celestialcrafts/settings.py
@@ -178,7 +178,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'celestial-crafts'
@@ -196,7 +195,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
 
 #stripe
 
