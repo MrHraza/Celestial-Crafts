f56396a8fcb5038366fe51262a3341fd8dea931d
Husnain Raza <hr_11@hotmail.co.uk>
2024-08-19 20:45:28 +0000

    manual testing updated

--- checkout/models.py
+++ checkout/models.py
@@ -13,24 +13,6 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80)
     street_address2 = models.CharField(max_length=80, blank=True, null=True)
     county = models.CharField(max_length=80, blank=True, null=True)
-    date = models.DateTimeField(auto_now_add=True)  
-    total_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-
-    def __str__(self):from django.db import models
-from products.models import Product
-from django.utils import timezone
-
-
-class Order(models.Model):
-    full_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    country = models.CharField(max_length=40)
-    postcode = models.CharField(max_length=20)
-    town_or_city = models.CharField(max_length=40)
-    street_address1 = models.CharField(max_length=80)
-    street_address2 = models.CharField(max_length=80, blank=True, null=True)
-    county = models.CharField(max_length=80, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     total_cost = models.DecimalField(max_digits=10,
                                      decimal_places=2, default=0.00)
@@ -49,13 +31,5 @@ class OrderItem(models.Model):
     def __str__(self):
         return f'{self.quantity} of {self.product.name}'
 
-        return self.full_name
+      
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f'{self.quantity} of {self.product.name}'
