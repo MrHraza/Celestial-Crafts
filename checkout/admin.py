c6e7c51087e9d62f2770bcf1e853ad47c14ad514
Husnain Raza <hr_11@hotmail.co.uk>
2024-08-19 19:25:00 +0000

    screenshots of website

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     fields = ('product', 'quantity', 'price')  
     extra = 0  
     readonly_fields = ('product', 'quantity', 'price')  
+
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderItemInline,)
@@ -13,5 +15,6 @@ class OrderAdmin(admin.ModelAdmin):
               'country', 'postcode', 'town_or_city',
               'street_address1', 'street_address2', 'county')
     list_display = ('full_name', 'email', 'phone_number')
+
 
 admin.site.register(Order, OrderAdmin)
